a96964cb0a937ccfb2fa6539fdbf6846a573c10a
延皓 <harvey_husky@foxmail.com>
2025-02-16 22:41:00 +0800

    暂时修复一个崩溃问题，需要进一步测试。

--- src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/MixinSoundEngine.java
+++ src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/MixinSoundEngine.java
@@ -1,19 +1,38 @@
 package fun.qu_an.minecraft.asyncparticles.client.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import fun.qu_an.minecraft.asyncparticles.client.AsyncTicker;
 import net.minecraft.client.sounds.SoundEngine;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SoundEngine.class)
 public class MixinSoundEngine {
-//	@Inject(method = "tick", at = @At("HEAD"), cancellable = true)
-//	private void onTick(boolean bl, CallbackInfo ci) {
-//		// TODO: 是否可以异步？（一些 mod 频繁播放音效会导致卡顿）
-//		if (!AsyncTicker.shouldTickParticles) {
-//			ci.cancel();
-//		}
-//	}
+	@Shadow
+	@Final
+	private static Logger LOGGER;
+
+	@Inject(method = "tick", at = @At("HEAD"), cancellable = true)
+	private void onTick(boolean bl, CallbackInfo ci) {
+		// TODO: 是否可以异步？（一些 mod 频繁播放音效会导致卡顿）
+		if (!AsyncTicker.shouldTickParticles) {
+			ci.cancel();
+		}
+	}
+
+	@WrapMethod(method = "tick")
+	private void wrapTick(boolean bl, Operation<Void> original) {
+		try {
+			// FIXME: 查明原因
+			original.call(bl);
+		} catch (NullPointerException e) {
+			LOGGER.error("Error while ticking particles", e);
+		}
+	}
 }
