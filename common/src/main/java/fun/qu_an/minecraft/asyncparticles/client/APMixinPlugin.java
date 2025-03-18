cacb95c85109e9d1936e82c990237981e5e340b9
延皓 <harvey_husky@foxmail.com>
2025-03-18 22:30:09 +0800

    fix a crash with modernui

--- common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/APMixinPlugin.java
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/APMixinPlugin.java
@@ -72,6 +72,7 @@ public class APMixinPlugin implements IMixinConfigPlugin {
 			}
 			case "fake_renders",
 				 "off_thread_access" -> true;
+			case "modernui" -> ModListHelper.MODERN_UI_LOADED;
 			case "vs2" -> ModListHelper.VS_LOADED;
 			case "create" -> ModListHelper.CREATE_LOADED;
 			case "iris_like" -> ModListHelper.IRIS_LIKE_LOADED;
