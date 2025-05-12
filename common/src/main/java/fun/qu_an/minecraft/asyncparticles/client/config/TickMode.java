5fbc1504ef18cadcec976ba683b15e0803d28458
延皓 <harvey_husky@foxmail.com>
2025-05-12 23:13:40 +0800

    alpha.3;

--- common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/config/TickMode.java
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/config/TickMode.java
@@ -1,14 +1,18 @@
 package fun.qu_an.minecraft.asyncparticles.client.config;
 
 import fun.qu_an.minecraft.asyncparticles.client.util.TranslatableEnum;
+import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 
 import java.util.function.Supplier;
 
 public enum TickMode implements TranslatableEnum {
-	SYNCHRONOUSLY(() -> Component.translatable("config.asyncparticles.enum.TickMode.SYNCHRONOUSLY")),
-	INTERRUPTIBLE(() -> Component.translatable("config.asyncparticles.enum.TickMode.INTERRUPTIBLE")),
-	FORCE_COMPLETE(() -> Component.translatable("config.asyncparticles.enum.TickMode.FORCE_COMPLETE"));
+	SYNCHRONOUSLY(() -> Component.translatable("config.asyncparticles.enum.TickMode.SYNCHRONOUSLY")
+		.withStyle(ChatFormatting.RED)),
+	INTERRUPTIBLE(() -> Component.translatable("config.asyncparticles.enum.TickMode.INTERRUPTIBLE")
+		.withStyle(ChatFormatting.GREEN)),
+	FORCE_COMPLETE(() -> Component.translatable("config.asyncparticles.enum.TickMode.FORCE_COMPLETE")
+		.withStyle(ChatFormatting.YELLOW));
 	private final Supplier<Component> componentSupplier;
 
 	TickMode(Supplier<Component> componentSupplier) {
