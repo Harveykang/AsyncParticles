0106f42e5a1682dfa961efbd49654466d7792397
延皓 <harvey_husky@foxmail.com>
2025-05-31 20:01:34 +0800

    subtle effect + Weapons of Miracle compatibility

--- common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/subtle_effects/MixinAsyncRenderer.java
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/subtle_effects/MixinAsyncRenderer.java
@@ -6,6 +6,7 @@ import fun.qu_an.minecraft.asyncparticles.client.compat.subtle_effects.SubtleEff
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.Particle;
+import net.minecraft.client.particle.ParticleRenderType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
@@ -14,7 +15,9 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 public class MixinAsyncRenderer {
 	@Redirect(method = "renderParticles",
 		at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/Particle;isAlive()Z"))
-	private static boolean shouldRenderParticle(Particle instance, @Local(argsOnly = true) Camera camera) {
-		return instance.isAlive() && SubtleEffectsCompat.shouldRenderParticle(instance, camera, Minecraft.getInstance().level);
+	private static boolean shouldRenderParticle(Particle instance,
+												@Local(argsOnly = true) Camera camera,
+												@Local(argsOnly = true) ParticleRenderType renderType) {
+		return instance.isAlive() && SubtleEffectsCompat.shouldRenderParticle(renderType, instance, camera, Minecraft.getInstance().level);
 	}
 }
