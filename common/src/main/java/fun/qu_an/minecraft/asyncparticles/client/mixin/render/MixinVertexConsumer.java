359344b298f4ac0c477240ede7dff9132385004b
延皓 <harvey_husky@foxmail.com>
2025-06-24 03:32:18 +0800

    fix a potential crash

--- common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/render/MixinVertexConsumer.java
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/render/MixinVertexConsumer.java
@@ -2,14 +2,22 @@ package fun.qu_an.minecraft.asyncparticles.client.mixin.render;
 
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
 
 @Mixin(VertexConsumer.class)
 public interface MixinVertexConsumer {
-	@ModifyArg(method = "color(FFFF)Lcom/mojang/blaze3d/vertex/VertexConsumer;", index = 3,
-		at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/VertexConsumer;color(IIII)Lcom/mojang/blaze3d/vertex/VertexConsumer;"))
-	default int fixNegativeAlpha(int alpha) {
-		return Math.max(alpha, 0);
+	@Shadow VertexConsumer color(int red, int green, int blue, int alpha);
+
+	/**
+	 * @author Harvey_Husky
+	 * @reason Fix negative particle alpha.
+	 * I have no idea why 'default' and 'private' injections are causing crash in different mixin environments (on Forge).
+	 * And it has NEVER crashed my game, either on Forge or Fabric. So I have to use '@Overwrite' instead.
+	 */
+	@Overwrite
+	default VertexConsumer color(float red, float green, float blue, float alpha) {
+		return this.color((int) (red * 255.0F), (int) (green * 255.0F), (int) (blue * 255.0F),
+			Math.max(0, (int) (alpha * 255.0F)));
 	}
 }
