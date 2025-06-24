0125de51c7e361ff4208ec70b8b134d5d5b5bdfd
延皓 <harvey_husky@foxmail.com>
2025-06-25 06:40:52 +0800

    fix negative alpha; fix potential mixin conflict in ActiveProfiler

---
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/render/MixinVertexConsumer.java
@@ -0,0 +1,15 @@
+package fun.qu_an.minecraft.asyncparticles.client.mixin.render;
+
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+@Mixin(VertexConsumer.class)
+public interface MixinVertexConsumer {
+	@ModifyArg(method = "setColor(FFFF)Lcom/mojang/blaze3d/vertex/VertexConsumer;", index = 3,
+		at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/VertexConsumer;setColor(IIII)Lcom/mojang/blaze3d/vertex/VertexConsumer;"))
+	private int fixNegativeAlpha(int alpha) {
+		return Math.max(alpha, 0);
+	}
+}
