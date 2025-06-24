01cd4e228ea74612bebcaa6726a6d50b8858116a
延皓 <harvey_husky@foxmail.com>
2025-06-25 06:28:23 +0800

    fix potential mixin conflict in ActiveProfiler

--- common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/tick/MixinParticleEngine.java
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/tick/MixinParticleEngine.java
@@ -156,6 +156,7 @@ public abstract class MixinParticleEngine {
 			return;
 		}
 		boolean enableLightCache = ConfigHelper.particleLightCache();
+		boolean isNotOnMainThread = !ThreadUtil.isOnMainThread();
 		for (Particle particle : collection) {
 			if (AsyncTicker.isCancelled() && !ConfigHelper.forceDoneParticleTick()) {
 				return;
@@ -166,7 +167,7 @@ public abstract class MixinParticleEngine {
 				Utils.DUMMY_ITERATOR.remove();
 				continue;
 			}
-			if (!ThreadUtil.isOnMainThread()) {
+			if (isNotOnMainThread) {
 				if (((ParticleAddon) particle).asyncparticles$isTicked()) {
 					// Skip the first tick that the particle is added to the queue.
 					if (enableLightCache) {
