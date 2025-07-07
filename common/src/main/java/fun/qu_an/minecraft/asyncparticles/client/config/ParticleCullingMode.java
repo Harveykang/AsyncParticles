8da74b57a8de8c1882477630fb55fda70630a9ab
延皓 <harvey_husky@foxmail.com>
2025-07-07 16:09:22 +0800

    beta!

--- common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/config/ParticleCullingMode.java
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/config/ParticleCullingMode.java
@@ -7,15 +7,15 @@ import net.minecraft.network.chat.Component;
 import java.util.function.Supplier;
 
 public enum ParticleCullingMode implements TranslatableEnum {
-	DISABLED(() -> Component.translatable("config.asyncparticles.enum.ParticleCullingShape.DISABLED")
+	DISABLED(() -> Component.translatable("config.asyncparticles.enum.ParticleCullingMode.DISABLED")
 		.withStyle(ChatFormatting.RED)),
-	SPHERE(() -> Component.translatable("config.asyncparticles.enum.ParticleCullingShape.SPHERE")
+	SPHERE(() -> Component.translatable("config.asyncparticles.enum.ParticleCullingMode.SPHERE")
 		.withStyle(ChatFormatting.GREEN)),
-	AABB(() -> Component.translatable("config.asyncparticles.enum.ParticleCullingShape.AABB")
+	AABB(() -> Component.translatable("config.asyncparticles.enum.ParticleCullingMode.AABB")
 		.withStyle(ChatFormatting.YELLOW)),
-	ASYNC_SPHERE(() -> Component.translatable("config.asyncparticles.enum.ParticleCullingShape.ASYNC_SPHERE")
+	ASYNC_SPHERE(() -> Component.translatable("config.asyncparticles.enum.ParticleCullingMode.ASYNC_SPHERE")
 		.withStyle(ChatFormatting.GREEN)),
-	ASYNC_AABB(() -> Component.translatable("config.asyncparticles.enum.ParticleCullingShape.ASYNC_AABB")
+	ASYNC_AABB(() -> Component.translatable("config.asyncparticles.enum.ParticleCullingMode.ASYNC_AABB")
 		.withStyle(ChatFormatting.YELLOW));
 	private final Supplier<Component> componentSupplier;
 
