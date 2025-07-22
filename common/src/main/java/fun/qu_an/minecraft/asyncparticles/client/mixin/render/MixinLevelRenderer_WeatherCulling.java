5f67ee527519f8082bf192148193cb77de3220d6
延皓 <harvey_husky@foxmail.com>
2025-07-22 11:08:53 +0800

    fix mixin conflict

--- common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/render/MixinLevelRenderer_WeatherCulling.java
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/render/MixinLevelRenderer_WeatherCulling.java
@@ -8,7 +8,8 @@ import com.llamalad7.mixinextras.sugar.ref.LocalBooleanRef;
 import com.llamalad7.mixinextras.sugar.ref.LocalIntRef;
 import fun.qu_an.minecraft.asyncparticles.client.config.ConfigHelper;
 import fun.qu_an.minecraft.asyncparticles.client.util.FrustumUtil;
-import net.minecraft.client.renderer.*;
+import net.minecraft.client.renderer.LevelRenderer;
+import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
@@ -20,7 +21,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = LevelRenderer.class, priority = 1500)
@@ -31,32 +31,32 @@ public abstract class MixinLevelRenderer_WeatherCulling {
 	private static final Holder<Biome> asyncparticles$NULL = Holder.direct(null);
 
 	@Inject(method = "renderSnowAndRain", at = @At(value = "HEAD"))
-	public void beforeRenderSnowAndRain(CallbackInfo ci, @Share("enableCull") LocalBooleanRef enableCull){
+	public void beforeRenderSnowAndRain(CallbackInfo ci, @Share("enableCull") LocalBooleanRef enableCull) {
 		enableCull.set(ConfigHelper.isCullWeathers());
 	}
 
-	@Inject(method = "renderSnowAndRain", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getBiome(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/core/Holder;"))
+	@Inject(method = "renderSnowAndRain", at = @At(value = "INVOKE",
+		target = "Lnet/minecraft/world/level/Level;getBiome(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/core/Holder;"))
 	public void beforeGetBiome(LightTexture lightTexture,
-								  float partialTick,
-								  double camX,
-								  double camY,
-								  double camZ,
-								  CallbackInfo ci,
-								  @Local(ordinal = 0) Level level,
-								  @Local(ordinal = 0) BlockPos.MutableBlockPos mutableBlockPos,
-								  @Local(ordinal = 1) int j,
-								  @Local(ordinal = 3) int l,
-								  @Local(ordinal = 5) int n,
-								  @Local(ordinal = 6) int o,
-								  @Share(value = "height_map") LocalIntRef qRef,
-								  @Share(value = "isVisible") LocalBooleanRef isVisible,
-								  @Share(value = "enableCull") LocalBooleanRef enableCull) {
-		int q = level.getHeight(Heightmap.Types.MOTION_BLOCKING, o, n);
+							   float partialTick,
+							   double camX,
+							   double camY,
+							   double camZ,
+							   CallbackInfo ci,
+							   @Local(ordinal = 0) Level level,
+							   @Local(ordinal = 0) BlockPos.MutableBlockPos mutableBlockPos,
+							   @Local(ordinal = 1) int j,
+							   @Local(ordinal = 3) int l,
+							   @Local(ordinal = 5) int n,
+							   @Local(ordinal = 6) int o,
+							   @Share(value = "height_map") LocalIntRef qRef,
+							   @Share(value = "isVisible") LocalBooleanRef isVisible,
+							   @Share(value = "enableCull") LocalBooleanRef enableCull) {
 		if (!enableCull.get()) {
-			qRef.set(q);
 			isVisible.set(true);
 			return;
 		}
+		int q = level.getHeight(Heightmap.Types.MOTION_BLOCKING, o, n);
 		int s = j + l;
 		if (s < q) {
 			isVisible.set(false);
@@ -74,25 +74,18 @@ public abstract class MixinLevelRenderer_WeatherCulling {
 		}
 	}
 
-	@WrapOperation(method = "renderSnowAndRain", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getBiome(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/core/Holder;"))
+	@WrapOperation(method = "renderSnowAndRain", at = @At(value = "INVOKE",
+		target = "Lnet/minecraft/world/level/Level;getBiome(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/core/Holder;"))
 	public Holder<Biome> wrapGetBiome(Level instance, BlockPos pos,
-								  Operation<Holder<Biome>> original,
-								  @Share(value = "isVisible") LocalBooleanRef isVisible) {
+									  Operation<Holder<Biome>> original,
+									  @Share(value = "isVisible") LocalBooleanRef isVisible) {
 		return isVisible.get() ? original.call(instance, pos) : asyncparticles$NULL;
 	}
 
-	@WrapOperation(method = "renderSnowAndRain", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/biome/Biome;hasPrecipitation()Z"))
+	@WrapOperation(method = "renderSnowAndRain", at = @At(value = "INVOKE",
+		target = "Lnet/minecraft/world/level/biome/Biome;hasPrecipitation()Z"))
 	public boolean shouldRenderWeatherColumn(Biome instance, Operation<Boolean> original,
-									@Share(value = "isVisible") LocalBooleanRef isVisible) {
+											 @Share(value = "isVisible") LocalBooleanRef isVisible) {
 		return isVisible.get() && original.call(instance);
-	}
-
-	@Redirect(method = "renderSnowAndRain", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/Level;getHeight(Lnet/minecraft/world/level/levelgen/Heightmap$Types;II)I"))
-	public int onGetHeight(Level instance,
-						 Heightmap.Types types,
-						 int i,
-						 int j,
-						 @Share(value = "height_map") LocalIntRef qRef) {
-		return qRef.get();
 	}
 }
