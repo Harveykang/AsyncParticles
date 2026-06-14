6ef6f31eda974efc56e8249a311021c2289afc9a
延皓 <harvey_husky@foxmail.com>
2026-06-14 22:48:40 +0800

    ready to release

--- common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/core/particle/tick/MixinElderGuardianParticleGroup$ElderGuardianParticleRenderState.java
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/core/particle/tick/MixinElderGuardianParticleGroup$ElderGuardianParticleRenderState.java
@@ -1,28 +1,19 @@
 package fun.qu_an.minecraft.asyncparticles.client.mixin.core.particle.tick;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalFloatRef;
 import fun.qu_an.minecraft.asyncparticles.client.addon.ParticleAddon;
-import net.minecraft.client.Camera;
 import net.minecraft.client.particle.ElderGuardianParticle;
 import net.minecraft.client.particle.ElderGuardianParticleGroup;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ElderGuardianParticleGroup.class)
 public class MixinElderGuardianParticleGroup$ElderGuardianParticleRenderState {
-	@Inject(method = "lambda$extractRenderState$0", at = @At("HEAD"))
-	private static void modifyParticleRecord(Camera camera,
-	                                         float partialTickTime,
-	                                         ElderGuardianParticle particle,
-	                                         CallbackInfoReturnable<?> cir,
-	                                         @Local(argsOnly = true, name = "partialTickTime") LocalFloatRef tickDelta) {
-
-		if (!((ParticleAddon) particle).asyncparticles$isTicked() && partialTickTime <= 1.0f) {
-			float v = partialTickTime + 1.0f;
-			tickDelta.set(v);
-		}
+	@ModifyArg(method = "lambda$extractRenderState$0", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ElderGuardianParticleGroup$ElderGuardianParticleRenderState;fromParticle(Lnet/minecraft/client/particle/ElderGuardianParticle;Lnet/minecraft/client/Camera;F)Lnet/minecraft/client/particle/ElderGuardianParticleGroup$ElderGuardianParticleRenderState;"))
+	private static float modifyParticleRecord(float partialTickTime,
+	                                          @Local(argsOnly = true, name = "particle")
+											  ElderGuardianParticle particle) {
+		return !((ParticleAddon) particle).asyncparticles$isTicked() && partialTickTime <= 1.0f ? partialTickTime + 1.0f : partialTickTime;
 	}
 }
--- common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/core/particle/tick/MixinItemPickupParticleGroup$ParticleInstance.java
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/core/particle/tick/MixinItemPickupParticleGroup$ParticleInstance.java
@@ -1,28 +1,19 @@
 package fun.qu_an.minecraft.asyncparticles.client.mixin.core.particle.tick;
 
 import com.llamalad7.mixinextras.sugar.Local;
-import com.llamalad7.mixinextras.sugar.ref.LocalFloatRef;
 import fun.qu_an.minecraft.asyncparticles.client.addon.ParticleAddon;
-import net.minecraft.client.Camera;
 import net.minecraft.client.particle.ItemPickupParticle;
 import net.minecraft.client.particle.ItemPickupParticleGroup;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(ItemPickupParticleGroup.class)
 public class MixinItemPickupParticleGroup$ParticleInstance {
-	@Inject(method = "lambda$extractRenderState$0", at = @At("HEAD"))
-	private static void modifyParticleRecord(Camera camera,
-	                                         float partialTickTime,
-	                                         ItemPickupParticle particle,
-	                                         CallbackInfoReturnable<?> cir,
-											 @Local(argsOnly = true, name = "partialTickTime") LocalFloatRef tickDelta) {
-
-		if (!((ParticleAddon) particle).asyncparticles$isTicked() && partialTickTime <= 1.0f) {
-			float v = partialTickTime + 1.0f;
-			tickDelta.set(v);
-		}
+	@ModifyArg(method = "lambda$extractRenderState$0", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/particle/ItemPickupParticleGroup$ParticleInstance;fromParticle(Lnet/minecraft/client/particle/ItemPickupParticle;Lnet/minecraft/client/Camera;F)Lnet/minecraft/client/particle/ItemPickupParticleGroup$ParticleInstance;"))
+	private static float modifyParticleRecord(float partialTickTime,
+	                                          @Local(argsOnly = true, name = "particle")
+											  ItemPickupParticle particle) {
+		return !((ParticleAddon) particle).asyncparticles$isTicked() && partialTickTime <= 1.0f ? partialTickTime + 1.0f : partialTickTime;
 	}
 }
