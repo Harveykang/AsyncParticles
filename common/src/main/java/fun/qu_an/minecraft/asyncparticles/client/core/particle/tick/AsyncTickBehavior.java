983e07239148e94d2e15eae720b4591cd18da577
延皓 <harvey_husky@foxmail.com>
2026-08-07 05:39:12 +0800

    fix particle glitch when `Async Particle Tick` is disabled and `Append New Particles to GPU Renderer` is enabled; remove `ParticleCleanupStrategy.AFTER_ASYNC_TICK`

--- common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/core/particle/tick/AsyncTickBehavior.java
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/core/particle/tick/AsyncTickBehavior.java
@@ -225,10 +225,6 @@ public class AsyncTickBehavior {
 			particlePhase = true;
 			mc.particleEngine.tick();
 			particlePhase = false;
-			if (cleanupStrategy == ParticleCleanupStrategy.AFTER_ASYNC_TICK) {
-				tickTaskHelper.groupTasks(false);
-				prepareCleanupTasks(tickTaskHelper);
-			}
 		}
 		tickTaskHelper.submitAll(() -> {
 			timeUsageNano.setRelease(System.nanoTime());
--- common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/core/particle/gpu_acceleration/MixinParticleEngine.java
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/mixin/core/particle/gpu_acceleration/MixinParticleEngine.java
@@ -37,8 +37,11 @@ public abstract class MixinParticleEngine implements ParticleEngineAddon {
 		if (!ConfigHelper.isGpuParticles()) {
 			return;
 		}
-		int sum = 0;
 		AsyncTickBehavior tickBehavior = AsyncTickBehavior.getInstance();
+		if (tickBehavior.isTailTick()) {
+			GpuParticleBehavior.getInstance().flushBufferAndSwap();
+		}
+		int sum = 0;
 		boolean tickAsync = ConfigHelper.isAsyncParticleTick() && tickBehavior.isParticlePhase();
 		TaskHelper taskHelper = tickBehavior.getTickTaskManager();
 		for (ParticleGroup<?> group : particles.values()) {
@@ -66,7 +69,6 @@ public abstract class MixinParticleEngine implements ParticleEngineAddon {
 			return;
 		}
 		GpuParticleBehavior gpuParticleBehavior = GpuParticleBehavior.getInstance();
-		gpuParticleBehavior.flushBufferAndSwap();
 		gpuParticleBehavior.setUpNextTickRendering(sum);
 		IParticleRenderer renderer = gpuParticleBehavior.getOrCreateRenderer();
 		renderer.prepareBuffer();
