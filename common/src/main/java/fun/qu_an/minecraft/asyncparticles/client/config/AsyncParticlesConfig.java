4d5dd20fb69e25e2f5ea82fc7e6bb68a5625781f
延皓 <harvey_husky@foxmail.com>
2026-08-11 08:12:01 +0800

    Move the button to the top-right corner. preserve particleLimit

--- common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/config/AsyncParticlesConfig.java
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/config/AsyncParticlesConfig.java
@@ -321,16 +321,19 @@ public class AsyncParticlesConfig {
 			public boolean equals(Object o) {
 				if (o == null || getClass() != o.getClass()) return false;
 				Particle particle = (Particle) o;
-				return particleLimit == particle.particleLimit
-					&& parallelQueueRemoval == particle.parallelQueueRemoval
-					&& particleLightCache == particle.particleLightCache
-					&& cullUnderwaterParticleType == particle.cullUnderwaterParticleType
-					&& cleanupStrategy == particle.cleanupStrategy;
+				return
+//					particleLimit == particle.particleLimit &&
+					parallelQueueRemoval == particle.parallelQueueRemoval
+						&& particleLightCache == particle.particleLightCache
+						&& cullUnderwaterParticleType == particle.cullUnderwaterParticleType
+						&& cleanupStrategy == particle.cleanupStrategy;
 			}
 
 			@Override
 			public int hashCode() {
-				return Objects.hash(particleLimit, cleanupStrategy, parallelQueueRemoval, particleLightCache, cullUnderwaterParticleType);
+				return Objects.hash(
+//					particleLimit,
+					cleanupStrategy, parallelQueueRemoval, particleLightCache, cullUnderwaterParticleType);
 			}
 		}
 
@@ -382,12 +385,22 @@ public class AsyncParticlesConfig {
 					&& deferredTextureTick == tick.deferredTextureTick
 					&& failPerSecLimit == tick.failPerSecLimit
 					&& suppressCME == tick.suppressCME
-					&& particleAsyncMode == tick.particleAsyncMode;
+					&& particleAsyncMode == tick.particleAsyncMode
+//					&& syncParticleClasses.equals(tick.syncParticleClasses)
+					;
 			}
 
 			@Override
 			public int hashCode() {
-				return Objects.hash(animationTickMode, particleAsyncMode, gpuOnlyAsyncParticleTick, tickWeatherAsync, deferredTextureTick, failPerSecLimit, suppressCME);
+				return Objects.hash(animationTickMode,
+					particleAsyncMode,
+					gpuOnlyAsyncParticleTick,
+					tickWeatherAsync,
+					deferredTextureTick,
+					failPerSecLimit,
+					suppressCME
+//					, syncParticleClasses
+				);
 			}
 		}
 
