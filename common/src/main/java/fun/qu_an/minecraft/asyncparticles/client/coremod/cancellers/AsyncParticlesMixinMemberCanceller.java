10523816f56fd4d40d696771be96823842f2bfaa
延皓 <harvey_husky@foxmail.com>
2026-08-19 15:05:54 +0800

    RuOK compat

--- common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/coremod/cancellers/AsyncParticlesMixinMemberCanceller.java
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/coremod/cancellers/AsyncParticlesMixinMemberCanceller.java
@@ -11,6 +11,7 @@ public class AsyncParticlesMixinMemberCanceller implements MixinMemberCanceller 
 			case "einstein.subtle_effects.mixin.client.particle.FabricParticleEngineMixin",
 				 "einstein.subtle_effects.mixin.client.particle.ForgeParticleEngineMixin",
 				 "team.teampotato.ruok.mixin.minecraft.ParticleManagerMixin",
+				 "team.teampotato.ruok.mixin.minecraft.particle.ParticleManagerMixin",
 				 "com.moepus.flerovium.mixins.Particle.SingleQuadParticleMixin",
 				 "io.github.fabricators_of_create.porting_lib.mixin.client.ParticleEngineMixin",
 				 "qouteall.imm_ptl.core.mixin.client.particle.MixinParticleEngine",
@@ -28,8 +29,6 @@ public class AsyncParticlesMixinMemberCanceller implements MixinMemberCanceller 
 			case "einstein.subtle_effects.mixin.client.particle.FabricParticleEngineMixin",
 				 "einstein.subtle_effects.mixin.client.particle.ForgeParticleEngineMixin" ->
 				"shouldRenderParticle".equals(mixinMethodName);
-			case "team.teampotato.ruok.mixin.minecraft.ParticleManagerMixin" ->
-				"tick".equals(mixinMethodName); // TODO update RuOK to a new version
 			case "com.moepus.flerovium.mixins.Particle.SingleQuadParticleMixin" ->
 				"flerovium$getLightColorCached".equals(mixinMethodName);
 			case "io.github.fabricators_of_create.porting_lib.mixin.client.ParticleEngineMixin" ->
@@ -40,6 +39,10 @@ public class AsyncParticlesMixinMemberCanceller implements MixinMemberCanceller 
 				"onTickParticle".equals(mixinMethodName);
 			case "me.fzzyhmstrs.particle_core.mixins.ParticleManagerAsyncMixin" ->
 				"particle_core_synchronizeParticleAdds".equals(mixinMethodName);
+			case "team.teampotato.ruok.mixin.minecraft.ParticleManagerMixin" ->
+				"tick".equals(mixinMethodName);
+			case "team.teampotato.ruok.mixin.minecraft.particle.ParticleManagerMixin" ->
+				"tick".equals(mixinMethodName);
 			default -> false;
 		};
 	}
