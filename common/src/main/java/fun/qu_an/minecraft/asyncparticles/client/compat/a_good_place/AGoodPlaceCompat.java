b2748d3d76ab696aae983f3cb89aff9cb800c8bb
延皓 <harvey_husky@foxmail.com>
2026-09-08 12:14:32 +0800

    well... i think i forgot this

--- common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/compat/a_good_place/AGoodPlaceCompat.java
+++ common/src/main/java/fun/qu_an/minecraft/asyncparticles/client/compat/a_good_place/AGoodPlaceCompat.java
@@ -1,12 +1,15 @@
 package fun.qu_an.minecraft.asyncparticles.client.compat.a_good_place;
 
+import fun.qu_an.minecraft.asyncparticles.client.core.particle.tick.LevelBundle;
 import fun.qu_an.minecraft.asyncparticles.client.mixin.compat.a_good_place.InvokerBlocksParticlesManager;
 import nl.enjarai.a_good_place.particles.BlocksParticlesManager;
 
 public class AGoodPlaceCompat {
 	public static void onParticleEngineClear() {
-		InvokerBlocksParticlesManager.accessor_getHiddenBlocks()
-			.forEach(InvokerBlocksParticlesManager::invoker_markBlockForRender);
+		if (LevelBundle.isLevelAvailable()) {
+			InvokerBlocksParticlesManager.accessor_getHiddenBlocks()
+				.forEach(InvokerBlocksParticlesManager::invoker_markBlockForRender);
+		}
 		BlocksParticlesManager.clear();
 	}
 }
